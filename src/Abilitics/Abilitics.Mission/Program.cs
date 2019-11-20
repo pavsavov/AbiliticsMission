4b163b7ff78de8ad6b5abc67ad69201af59bd5ea
Pavel <pav.savov@gmail.com>
2019-11-20 23:16:39 +0200

    project target framework changed to .NET Framework 4.7.2

--- src/Abilitics/Abilitics.Mission/Program.cs
+++ src/Abilitics/Abilitics.Mission/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Abilitics.Mission
 {
@@ -6,7 +10,6 @@ namespace Abilitics.Mission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
