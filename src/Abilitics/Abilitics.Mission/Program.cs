4f7c22eaff79bc9da6c0245948c324100a09e020
Pavel <pav.savov@gmail.com>
2019-11-21 00:25:26 +0200

    Create Database and Create Table draft methods

--- src/Abilitics/Abilitics.Mission/Program.cs
+++ src/Abilitics/Abilitics.Mission/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System;
+using System.Data.OleDb;
 using System.Threading.Tasks;
 
 namespace Abilitics.Mission
@@ -10,6 +12,73 @@ namespace Abilitics.Mission
     {
         static void Main(string[] args)
         {
+            Console.Write("Please, specify your .xlsx file path: ");
+
+            // var filePath = Console.ReadLine();
+
+            ImportToDb(/*filePath*/);
+        }
+
+        private static void ImportToDb(/*string filePath*/)
+        {
+            var filePath = @"E:\Projects\AbiliticsMission\src\Assignment.Description\";
+            //  var file = Directory.GetFiles(filePath);
+
+            var connectionString = @"Provider = 'Microsoft.ACE.OLEDB.12.0'; Data Source = '" + filePath + "'; Extended Properties = 'Excel 12.0 Xml;IMEX=1'";
+
+            CreateDatabase();
+
+            CreateTable();
+
+            //using (OleDbConnection oleDbConnection = new OleDbConnection(connectionString))
+            //{
+            //    oleDbConnection.CreateCommand
+            //}
+        }
+
+        private static void CreateDatabase()
+        {
+            var myConnectionString = @"Server=(localdb)\mssqllocaldb;Database=master;Integrated Security=True";
+
+            using (var connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "CREATE DATABASE AbiliticsMission";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Should use Async
+        /// </summary>
+        /// <returns></returns>
+        private static void CreateTable()
+        {
+            var myConnectionString = @"Server=(localdb)\mssqllocaldb;Database=master;Integrated Security=True";
+
+            using (var connection = new SqlConnection(myConnectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"CREATE TABLE dbo.Nobel
+                (
+                    Id int IDENTITY(1,1) NOT NULL,
+                    Year DateTime NOT NULL,
+                    Category NVARCHAR(255) NOT NULL,
+                    Name NVARCHAR(255) NOT NULL,
+                    Birthdate DATETIME NULL,
+                    Birth_Place NVARCHAR(255) NULL,
+                    Country NVARCHAR(255) NULL,
+                    Residence NVARCHAR(255) NULL,
+                    Field_Language NVARCHAR(255) NULL,
+                    Prize_Name NVARCHAR(255) NOT NULL,
+                    Motivation NVARCHAR(255) NOT NULL, 
+                    CONSTRAINT pk_id PRIMARY KEY (Id)
+                );";
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
