cec8953c5c1ac396a2b971cf557c9ca26c7b1b0f
Pavel <pav.savov@gmail.com>
2019-11-20 22:18:08 +0200

    Initial

---
+++ src/Abilitics/Abilitics.Mission/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abilitics.Mission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
