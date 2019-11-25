5260424507629eebd714286c72444edb20ba2256
Pavel <pav.savov@gmail.com>
2019-11-25 02:56:31 +0200

    Functional division of logic into smaller methods.Replaced 'magical strings'.

--- src/Abilitics/Abilitics.Mission/Common/SqlQueryContainer.cs
+++ src/Abilitics/Abilitics.Mission/Common/SqlQueryContainer.cs
@@ -34,17 +34,17 @@ namespace Abilitics.Mission.Common
         public string InsertRecordsInTable()
         {
             return $"INSERT INTO[dbo].[{configurations.DatabaseConfiguration["MainTable"]}] " +
-                        "           ([Year]," +
-                        "               [Category]," +
-                        "               [Name]," +
-                        "               [Birthdate]," +
-                        "               [Birth Place]," +
-                        "               [County]," +
-                        "               [Residence]," +
-                        "               [Field/Language]," +
-                        "               [Prize Name]," +
-                        "               [Motivation])" +
-                        "VALUES(@Year,@Category,@Name,@Birthdate,@BirthPlace,@County,@Residence,@FieldLanguage,@PrizeName,@Motivation) ";
+                       $"              ([{this.Year}]," +
+                       $"               [{this.Category}]," +
+                       $"               [{this.Name}]," +
+                       $"               [{this.Birthdate}]," +
+                       $"               [{this.BirthPlace}]," +
+                       $"               [{this.County}]," +
+                       $"               [{this.Residence}]," +
+                       $"               [{this.FieldLanguage}]," +
+                       $"               [{this.PrizeName}]," +
+                       $"               [{this.Motivation}])" +
+                        $"VALUES(@{nameof(this.Year)},@{nameof(this.Category)},@{nameof(this.Name)},@{nameof(this.Birthdate)},@{nameof(this.BirthPlace)},@{nameof(this.County)},@{nameof(this.Residence)},@{nameof(this.FieldLanguage)},@{nameof(this.PrizeName)},@{nameof(this.Motivation)}) ";
         }
 
         public string TruncateTable()
@@ -72,7 +72,17 @@ namespace Abilitics.Mission.Common
             return $"SELECT * FROM [dbo].[{configurations.DatabaseConfiguration["MainTable"]}]";
         }
 
-        //Table columns and sql parameters
+        //Table columns
+        public string Year { get; private set; } = "Year";
+        public string Category { get; private set; } = "Category";
+        public string Name { get; private set; } = "Name";
+        public string Birthdate { get; private set; } = "Birthdate";
+        public string BirthPlace { get; private set; } = "Birth Place";
+        public string County { get; private set; } = "County";
+        public string Residence { get; private set; } = "Residence";
+        public string FieldLanguage { get; private set; } = "Field/Language";
+        public string PrizeName { get; private set; } = "Prize Name";
+        public string Motivation { get; private set; } = "Motivation";
 
     }
 }
--- src/Abilitics/Abilitics.Mission/Program.cs
+++ src/Abilitics/Abilitics.Mission/Program.cs
@@ -52,7 +52,7 @@ namespace Abilitics.Mission
 
             try
             {
-                ImportToDb(configurations, sqlQueryContainer);
+                DatabaseRecordsImport(configurations, sqlQueryContainer);
             }
             catch (Exception ex)
             {
@@ -66,11 +66,23 @@ namespace Abilitics.Mission
         }
 
         /// <summary>
-        /// Read and Write from excel to DB
+        /// Logical connection between data load from excel file and data import to database.
         /// </summary>
-        /// <param name="configurations"></param>
-        /// <param name="sqlQueryContainer"></param>
-        private static void ImportToDb(ConfigurationModel configurations, SqlQueryContainer sqlQueryContainer)
+        /// <param name="configurations">DTO for access to .json configuration file.</param>
+        /// <param name="sqlQueryContainer">Holds Application SQL queries and database  operations related strings</param>
+        private static void DatabaseRecordsImport(ConfigurationModel configurations, SqlQueryContainer sqlQueryContainer)
+        {
+            var loadedDataFromExcelSheet = MapAndLoadFromExcelFile(configurations, sqlQueryContainer);
+
+            ImportToDatabase(configurations, sqlQueryContainer, loadedDataFromExcelSheet);
+        }
+
+        /// <summary>
+        /// Read and load data from .xlsx file into DataTable collection
+        /// </summary>
+        /// <param name="configurations">DTO for access to .json configuration file.</param>
+        /// <param name="sqlQueryContainer">Holds Application SQL queries and database  operations related strings</param>
+        private static DataTable MapAndLoadFromExcelFile(ConfigurationModel configurations, SqlQueryContainer sqlQueryContainer)
         {
             var excelData = new DataTable();
             var oleDbConnectionString = configurations.ExcelFileConfiguration["FileConnection"];
@@ -81,105 +93,123 @@ namespace Abilitics.Mission
 
                 var sheet = oleDbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
 
+
                 var columns = new DataColumn[]
                 {
-                    new DataColumn("Year",typeof(int)),
-                    new DataColumn("Category",typeof(string)),
-                    new DataColumn("Name",typeof(string)),
-                    new DataColumn("Birthdate",typeof(DateTime)), 
-					new DataColumn("Birth Place",typeof(string)),
-                    new DataColumn("County",typeof(string)),
-                    new DataColumn("Residence",typeof(string)),
-                    new DataColumn("Field/Language",typeof(string)),
-                    new DataColumn("Prize Name",typeof(string)),
-                    new DataColumn("Motivation",typeof(string))
+                    new DataColumn($"{sqlQueryContainer.Year}",typeof(int)),
+                    new DataColumn($"{sqlQueryContainer.Category}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.Name}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.Birthdate}",typeof(DateTime)),
+                    new DataColumn($"{sqlQueryContainer.BirthPlace}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.County}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.Residence}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.FieldLanguage}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.FieldLanguage}",typeof(string)),
+                    new DataColumn($"{sqlQueryContainer.Motivation}",typeof(string))
                 };
 
                 excelData.Columns.AddRange(columns);
 
                 var commandText = sqlQueryContainer.SelectExcelFileSheet(sheet);
 
+                //Map and Fill DataTable type object with records from excel table. 
                 using (OleDbDataAdapter oleAdapter = new OleDbDataAdapter(commandText, oleDbConnection))
                 {
-                    oleAdapter.TableMappings.Add("Year", "Year");
-                    oleAdapter.TableMappings.Add("Category", "Category");
-                    oleAdapter.TableMappings.Add("Name", "Name");
-                    oleAdapter.TableMappings.Add("Birthdate", "Birthdate"); 
-                    oleAdapter.TableMappings.Add("Birth Place", "Birth Place");
-                    oleAdapter.TableMappings.Add("County", "County");
-                    oleAdapter.TableMappings.Add("Residence", "Residence");
-                    oleAdapter.TableMappings.Add("Field/Language", "Field/Language");
-                    oleAdapter.TableMappings.Add("Prize Name", "Prize Name");
-                    oleAdapter.TableMappings.Add("Motivation", "Motivation");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Year}", $"{sqlQueryContainer.Year}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Category}", $"{sqlQueryContainer.Category}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Name}", $"{sqlQueryContainer.Name}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Birthdate}", $"{sqlQueryContainer.Birthdate}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.BirthPlace}", $"{sqlQueryContainer.BirthPlace}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.County}", $"{sqlQueryContainer.County}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Residence}", $"{sqlQueryContainer.Residence}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.FieldLanguage}", $"{sqlQueryContainer.FieldLanguage}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.PrizeName}", $"{sqlQueryContainer.PrizeName}");
+                    oleAdapter.TableMappings.Add($"{sqlQueryContainer.Motivation}", $"{sqlQueryContainer.Motivation}");
 
                     oleAdapter.Fill(excelData);
 
                 }
 
                 oleDbConnection.Close();
-                
-                //Connect to database in order to transfer data from the DataTable type object,loaded with data from .xlsx fil.
-                using (SqlConnection sqlConnection = new SqlConnection(configurations.ConnectionStrings["ApplicationConnection"]))
+
+                return excelData;
+            }
+        }
+
+        /// <summary>
+        /// Import loaded data from DataTable type collection into database table.
+        /// </summary>
+        /// <param name="configurations">DTO for access to .json configuration file.</param>
+        /// <param name="sqlQueryContainer">Holds Application SQL queries and database  operations related strings</param>
+        /// <param name="excelData">Mapped and loaded DataTable data structure with records from Excel sheet</param>
+        private static void ImportToDatabase(ConfigurationModel configurations, SqlQueryContainer sqlQueryContainer, DataTable excelData)
+        {
+            // Connect to database in order to transfer data from the DataTable type object, loaded with data from.xlsx fil.
+            using (SqlConnection sqlConnection = new SqlConnection(configurations.ConnectionStrings["ApplicationConnection"]))
+            {
+                var queryAllDataInDb = sqlQueryContainer.GetDataFromDbTable();
+                var connectionString = configurations.ConnectionStrings["ApplicationConnection"];
+                var currentDataInDb = PullData(connectionString, queryAllDataInDb);
+
+                var rows = currentDataInDb.Rows.Count > 0;
+
+                if (rows)
                 {
-                    var queryAllDataInDb = sqlQueryContainer.GetDataFromDbTable();
-                    var connectionString = configurations.ConnectionStrings["ApplicationConnection"];
+                    var truncateTableQuery = sqlQueryContainer.TruncateTable();
 
-                    var currentDataInDb = PullData(connectionString, queryAllDataInDb);
-
-                    var rows = currentDataInDb.Rows.Count > 0;
-
-                    if (rows)
+                    using (SqlCommand command = new SqlCommand(truncateTableQuery, sqlConnection))
                     {
-                        var truncateTableQuery = sqlQueryContainer.TruncateTable();
-
-                        using (SqlCommand command = new SqlCommand(truncateTableQuery, sqlConnection))
-                        {
-                            sqlConnection.Open();
-                            command.ExecuteNonQuery();
-                            sqlConnection.Close();
-                        }
+                        sqlConnection.Open();
+                        command.ExecuteNonQuery();
+                        sqlConnection.Close();
                     }
-
-                    // define INSERT query with parameters
-                    string query = sqlQueryContainer.InsertRecordsInTable();
-                    
-                    //count excel file rows;
-                    int rowCounter = 1;
-
-                    using (SqlCommand command = new SqlCommand(query, sqlConnection))
-                    {
-                        foreach (var row in excelData.AsEnumerable())
-                        {
-                            rowCounter++;
-
-                            command.Parameters.Add("@Year", SqlDbType.Int).Value = row.ItemArray[0] is DBNull ? (object)DBNull.Value : (int)row.ItemArray[0];
-                            command.Parameters.Add("@Category", SqlDbType.NVarChar, 255).Value = row.ItemArray[1] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[1];
-                            command.Parameters.Add("@Name", SqlDbType.NVarChar, 255).Value = row.ItemArray[2] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[2];
-                            command.Parameters.Add("@Birthdate", SqlDbType.NVarChar, 255).Value = row.ItemArray[3] is DBNull ? (object)DBNull.Value : DateFormatter(row.ItemArray[3], rowCounter);
-                            command.Parameters.Add("@BirthPlace", SqlDbType.NVarChar, 255).Value = row.ItemArray[4] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[4];
-                            command.Parameters.Add("@County", SqlDbType.NVarChar, 255).Value = row.ItemArray[5] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[5];
-                            command.Parameters.Add("@Residence", SqlDbType.NVarChar, 255).Value = row.ItemArray[6] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[6];
-                            command.Parameters.Add("@FieldLanguage", SqlDbType.NVarChar, 255).Value = row.ItemArray[7] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[7];
-                            command.Parameters.Add("@PrizeName", SqlDbType.NVarChar, 255).Value = row.ItemArray[8] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[8];
-                            command.Parameters.Add("@Motivation", SqlDbType.NVarChar, 255).Value = row.ItemArray[9] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[9];
-
-                            sqlConnection.Close();
-                            sqlConnection.Open();
-                            command.ExecuteNonQuery();
-                            sqlConnection.Close();
-
-                            command.Parameters.Clear();
-
-                        }
-                    }
-
-                    Console.WriteLine(ApplicationOutput());
                 }
+
+                // define INSERT query with parameters
+                string query = sqlQueryContainer.InsertRecordsInTable();
+
+                //count excel file rows;
+                int rowCounter = 1;
+
+                using (SqlCommand command = new SqlCommand(query, sqlConnection))
+                {
+                    foreach (var row in excelData.AsEnumerable())
+                    {
+                        rowCounter++;
+
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Year)}", SqlDbType.Int).Value = row.ItemArray[0] is DBNull ? (object)DBNull.Value : (int)row.ItemArray[0];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Category)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[1] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[1];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Name)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[2] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[2];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Birthdate)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[3] is DBNull ? (object)DBNull.Value : DateFormatter(row.ItemArray[3], rowCounter);
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.BirthPlace)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[4] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[4];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.County)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[5] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[5];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Residence)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[6] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[6];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.FieldLanguage)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[7] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[7];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.PrizeName)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[8] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[8];
+                        command.Parameters.Add($"@{nameof(sqlQueryContainer.Motivation)}", SqlDbType.NVarChar, 255).Value = row.ItemArray[9] is DBNull ? (object)DBNull.Value : (string)row.ItemArray[9];
+
+                        sqlConnection.Close();
+                        sqlConnection.Open();
+                        command.ExecuteNonQuery();
+                        sqlConnection.Close();
+
+                        command.Parameters.Clear();
+
+                    }
+                }
+
+                Console.WriteLine(ApplicationOutput());
             }
         }
 
         #region UtilityMethods
 
+        /// <summary>
+        /// Gets currently loaded records in database table
+        /// </summary>
+        /// <param name="connectionString">connection parameters for access to MSSQL Database</param>
+        /// <param name="query">Select SQL query for retrieving records</param>
+        /// <returns></returns>
         private static DataTable PullData(string connectionString, string query)
         {
             var dataTable = new DataTable();
